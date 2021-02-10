b0d73fc1f2d49d258f74b37a8f79499223f6bb0c
omihub777 <77219025+omihub777@users.noreply.github.com>
2021-02-10 16:15:40 +0900

    demo

--- model/preact50.py
+++ model/preact50.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 import tensorflow.keras.layers as layers
 
 class PreAct50(tf.keras.Model):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, weights='imagenet'):
         super(PreAct50, self).__init__()
-        self.net = tf.keras.applications.ResNet50V2(include_top=False, weights='imagenet', pooling="avg")
+        self.net = tf.keras.applications.ResNet50V2(include_top=False, weights=weights, pooling="avg")
         self.fc = layers.Dense(num_classes)
     def call(self, x):
         out = self.net(x)
@@ -15,7 +15,8 @@ class PreAct50(tf.keras.Model):
 if __name__ == "__main__":
     b,h,w,c = 4,224, 224, 3
     x = tf.random.normal((b,h,w,c))
-    net = PreAct50(10)
+    net = PreAct50(10, weights=None)
+    import IPython ; IPython.embed() ; exit(1)
     out =net(x)
     print(out.shape)
     net.summary()
